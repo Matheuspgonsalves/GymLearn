4c3478a8afc90ec70a30987019a6728f2f4d8905
unknown <matheus.gonsalvespereira@gmail.com>
2025-03-16 17:43:16 -0300

    Inicializando estrutura base do projeto GymLearn

---
+++ gymlearn-backend/src/test/java/com/gymlearn/gymlearn_backend/GymlearnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymlearn.gymlearn_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymlearnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
